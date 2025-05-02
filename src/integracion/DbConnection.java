aa2d7d2cf31f88e3e7c627d2970b7b57c08e175f
Nicolas Ucieda <nicolas.ucieda@gmail.com>
2025-05-02 21:56:37 +0200

    fix: deleted DB info

--- src/integracion/DbConnection.java
+++ src/integracion/DbConnection.java
@@ -8,9 +8,9 @@ public class DbConnection {
 
 private static Connection conn;
 
-private static final String URL = "jdbc:postgresql://localhost:5432/GestorAeropuerto";
-    private static final String USER = "nucieda";
-    private static final String PASSWORD = "pipi2003f";
+private static final String URL = "jdbc:postgresql://localhost:5432/";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         if (conn == null || conn.isClosed()) {
